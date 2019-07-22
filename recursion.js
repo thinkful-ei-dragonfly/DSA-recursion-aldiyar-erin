cf414549a03f1d42ea2d91d1fdcef4af9b0075e1
Aldiyar Batyrbekov <aldibatyrbekov@gmail.com>
2019-07-22 09:46:53 -0700

    maze

--- recursion.js
+++ recursion.js
@@ -4,7 +4,7 @@ const countSheeps = function (number) {
     return 'All sheep jumped over the fence';
   }
 
-  return `${number}: Another sheep jump over the fence `+countSheeps(number-1) 
+  return `${number}: Another sheep jump over the fence ` + countSheeps(number - 1)
 }
 
 // console.log(countSheeps(3))
@@ -19,8 +19,8 @@ const powerCalculator = function (integer, power) {
     return 'exponent should be >= 0';
   }
 
-  return integer*powerCalculator(integer, (power-1))
-  
+  return integer * powerCalculator(integer, (power - 1))
+
 
 }
 
@@ -34,13 +34,33 @@ const reverseStr = function (string) {
   }
 
   const lastChar = string[string.length - 1];
-  return lastChar + reverseStr(string.slice(0, string.length-1));
+  return lastChar + reverseStr(string.slice(0, string.length - 1));
 }
 
 // console.log(reverseStr('recursion'));
 
 
+let mySmallMaze = [
+  [' ', ' ', ' '],
+  [' ', '*', ' '],
+  [' ', ' ', 'e']
+];
 
 
 
+const maze = function (array) {
+  const exit = array[array.length - 1][array.length - 1];
+  const start = array[0][0];
 
+  
+
+  return { exit, start };
+}
+
+const array = [
+  ['S', 'R', 'R'],
+  ['D', '*', 'D'],
+  ['D', 'R', 'e']
+]
+
+console.log(maze(array))
