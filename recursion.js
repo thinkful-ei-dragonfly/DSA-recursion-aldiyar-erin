c7d2d3e67e43a8e956124ac5927f7feae5db1d3d
Aldiyar Batyrbekov <aldibatyrbekov@gmail.com>
2019-07-22 09:01:57 -0700

    nth tri

---
+++ recursion.js
@@ -0,0 +1,46 @@
+/* eslint-disable strict */
+const countSheeps = function (number) {
+  if (number === 0) {
+    return 'All sheep jumped over the fence';
+  }
+
+  return `${number}: Another sheep jump over the fence `+countSheeps(number-1) 
+}
+
+// console.log(countSheeps(3))
+
+
+const powerCalculator = function (integer, power) {
+  if (power === 1) {
+    return integer;
+  }
+
+  if (power < 0) {
+    return 'exponent should be >= 0';
+  }
+
+  return integer*powerCalculator(integer, (power-1))
+  
+
+}
+
+
+// console.log(powerCalculator(10, -1));
+
+const reverseStr = function (string) {
+
+  if (string === '') {
+    return '';
+  }
+
+  const lastChar = string[string.length - 1];
+  return lastChar + reverseStr(string.slice(0, string.length-1));
+}
+
+// console.log(reverseStr('recursion'));
+
+
+
+
+
+
